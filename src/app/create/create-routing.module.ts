2b3ce8689a4dca7f1b9e3b945c0db43709cf8a7b
隈本優希 <morikuma1019@kumamotoyuukinoMacBook-Air.local>
2020-04-21 14:33:03 +0900

    GitPet-Button

--- src/app/create/create-routing.module.ts
+++ src/app/create/create-routing.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './create/create.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
